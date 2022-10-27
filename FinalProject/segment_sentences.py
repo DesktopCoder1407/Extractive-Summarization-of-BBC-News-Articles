120609376d7ac17ba824794f192bfd76f0003023
DesktopCoder1407 <dcoder1407@gmail.com>
2022-10-26 22:23:25 -0400

    Automatic title creation

--- FinalProject/segment_sentences.py
+++ FinalProject/segment_sentences.py
@@ -5,7 +5,6 @@ import regex as re
 def segment_sentences(document: str, find_title: bool = False):
     title = None
     paragraphs = re.split(r'\n+', document)
-    sentences = []
 
     if find_title:
         sentences = sent_tokenize(paragraphs[0])
@@ -13,6 +12,7 @@ def segment_sentences(document: str, find_title: bool = False):
             title = sentences[0]
             paragraphs = paragraphs[1:]
 
+    sentences = []
     for paragraph in paragraphs:
         sentences.extend(sent_tokenize(paragraph))
 
--- FinalProject/tfidf_extractive_summary.py
+++ FinalProject/tfidf_extractive_summary.py
@@ -7,7 +7,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 def main():
     # The Corpus (List of all documents) and the text to be summarized (single document from corpus currently)
     corpus = [open(x, encoding='windows-1252').read() for x in glob.glob('data/raw_articles/*/*.txt')]  # TODO: Create a train/test split
-    text = corpus[1]
+    text = corpus[0]
 
     # Create the Tfidf vectorizer and fit the corpus to the vectorizer.
     vect = TfidfVectorizer(token_pattern=r"\$?\d*\.?\d+|\b\w\w+\b|\b[IiAa]\b")
@@ -18,15 +18,17 @@ def main():
     # Print out the N (in this case 6) highest scores as the final summariation.
     # Scoring method is currently tf_idf score divided by the number of words in the sentence. 
     # TODO: Modify scoring method. Possibly add similarity to title? Other heruistics?
-    print(text[0:text.find('\n')] + '\n-----') # Automatically prints title at top. Would prefer to not have.
+    title, scores = score_sentences(vect, text, True)
+    if title != None:
+        print(title + '\n') # Automatically prints title at top if it exists.
     for i in range(6):
-        print(get_score(vect, text)[i][1])
+        print(scores[i][1])
 
 
-def get_score(vectorizer, document):
+def score_sentences(vectorizer, document, look_for_title):
     scores = []
 
-    title, sentences = segment_sentences(document, True)
+    title, sentences = segment_sentences(document, look_for_title)
 
     for sentence in sentences:
         score = 0
@@ -37,7 +39,7 @@ def get_score(vectorizer, document):
         scores.append((score / word_count, sentence))
 
     scores.sort(reverse=True)
-    return scores
+    return title, scores
 
 
 if __name__ == '__main__':
