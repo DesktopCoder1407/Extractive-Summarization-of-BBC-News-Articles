eab40b0890ea3bff31e5e55ceea9d7ce759aa897
DesktopCoder1407 <dcoder1407@gmail.com>
2022-11-15 23:39:53 -0500

    begin work on features for naive bayes summarizer

--- FinalProject/naive_bayes_summary.py
+++ FinalProject/naive_bayes_summary.py
@@ -1,24 +1,61 @@
-import en_core_web_sm
 from glob import glob
-import matplotlib.pyplot as plt
-import statistics
-# Features: position of sentences in the document, sentence length, presence of uppercase words, similarity of the sentence to the document title, Uppercase Word Feature
+import re
 
-# P(s in S | Features) = P(features | s in S) / P(features)
-nlp = en_core_web_sm.load()
+# P(s in S | Features) = P(features | s in S) * P(sentence in Summary) [N/total_sentences]
 
-corpus_path = glob('data/raw_articles/business/*.txt')
-corpus_path.sort()
-corpus = [open(x, encoding='windows-1252').read() for x in corpus_path]
+def main():
+    corpus_path = glob('data/raw_articles/business/*.txt')
+    corpus_path.sort()
+    corpus_y_path = glob('data/summarized_articles/business/*.txt')
+    corpus_y_path.sort()
 
-title, text = corpus[0].split('\n', maxsplit=1)
-# text = text.replace('\n', '')
-doc = nlp(text)
-doc2 = nlp(open('data/summarized_articles/business/001.txt', encoding='windows-1252').read())
+    corpus = [open(x, encoding='windows-1252').read() for x in corpus_path]
+    corpus_y = [open(x, encoding='windows-1252').read() for x in corpus_y_path]
 
-for token in doc:
-    print(token)
+    title, doc = corpus[0].split('\n', maxsplit=1)
+    doc_y = corpus_y[0]
+
+    for vector in doc_to_feature_vector(title, doc, doc_y):
+        print(vector[0])
 
 
-# Sentence Length: <20, 20-35, >35 [???]
-# Paragraph Location: Beginning, Middle, End
+def doc_to_feature_vector(title, doc, doc_y):
+    doc_features = [] # FORMAT: SentenceLength, ParagraphLocation, SimilarityToTitle, UppercaseWords, CATEGORY
+    
+    paragraphs = re.findall(r".+", doc)
+    sentences = re.findall(r"[^ \n].+?\.(?!\d)", doc)
+
+    for sentence in sentences:
+        features = []
+        
+        # Sentence Length
+        sentence_len = len(re.findall(r"\S?\d+[.,]\d+\w+|[^ \n,.]+", sentence))
+        if sentence_len < 12:
+            features.append('short')
+        elif 12 <= sentence_len <= 25:
+            features.append('average')
+        else:
+            features.append('long')
+        
+        # Paragraph Location
+        for paragraph in paragraphs:
+            p_sents = re.findall(r"[^ \n].+?\.(?!\d)", paragraph)
+            for i, subsentence in enumerate(p_sents):
+                if sentence == subsentence:
+                    if i == 0:
+                        features.append('beginning')
+                    elif i == len(p_sents) - 1:
+                        features.append('end')
+                    else:
+                        features.append('middle')
+        
+        doc_features.append((features, sentence))
+    return doc_features
+
+
+if __name__ == "__main__":
+    main()
+
+# Similarity to Title: Contains >= 2 words in title, contains no words in title
+# Uppercase Words: Sentence contains >= 1(2) words that have an uppercase somewhere (ignore first word), contains no uppercase
+# ?Fixed Phrase: Sentence contains a phrase that is prespecified.
