2284f3c4d6797c1510c680dfe421f0285502edb5
DesktopCoder1407 <dcoder1407@gmail.com>
2022-11-16 16:21:12 -0500

    Clean up unused functions

--- FinalProject/data_description.py
+++ FinalProject/data_description.py
@@ -1,78 +1,36 @@
-import glob
-import spacy
-
-# Only includes lemmatizer, POS tagging, and detailed tag.
-nlp = spacy.load('en_core_web_sm', exclude=['tok2vec', 'parser', 'ner'])
-nlp.add_pipe('sentencizer', last=True)
-nlp.max_length = 5100000
-
-
-def directory_file_count():
-    file_list = glob.glob('data\\raw_articles\\*\\*.txt')
-    return f'File Count: {len(file_list)}'
-
-
-def get_corpus():
-    file_list = glob.glob('data/raw_articles/*/*.txt')
-    corpus = ''''''
-    for file in file_list:
-        with open(file, 'rt', encoding='windows-1252') as file_in:
-            corpus += file_in.read()
-    return corpus
-
+import tokenizer
+from glob import glob
 
 def get_corpus_split():
-    file_list = glob.glob('data/raw_articles/*/')
     corpus = []
-    for sub_folder in file_list:
-        document = ''''''
-        for file in glob.glob(sub_folder + '*.txt'):
-            with open(file, 'rt', encoding='windows-1252') as file_in:
-                document += file_in.read()
-        corpus.append(document)
+    for subfolder in ['business', 'entertainment', 'politics', 'sport', 'tech']:
+        corpus.append([open(x, encoding='windows-1252').read() for x in glob(f'data/raw_articles/{subfolder}/*.txt')])
     return corpus
 
-
-def sentences_count(corpus):
-    doc = nlp(corpus)
-
-    assert doc.has_annotation("SENT_START")
-    return len(list(doc.sents))
-
-
-full_corpus = get_corpus()
+# Get the Corpus
+corpus = [open(x, encoding='windows-1252').read() for x in glob('data/raw_articles/*/*.txt')]
 corpus_split = get_corpus_split()
-print(f'Total sentences in the business category: {sentences_count(corpus_split[0])}')
-print(f'Total sentences in the entertainment category: {sentences_count(corpus_split[1])}')
-print(f'Total sentences in the politics category: {sentences_count(corpus_split[2])}')
-print(f'Total sentences in the sport category: {sentences_count(corpus_split[3])}')
-print(f'Total sentences in the tech category: {sentences_count(corpus_split[4])}')
-print(f'Total sentences in the entire corpus: {sentences_count(full_corpus)}\n')
 
+# Sentences
+print(f'Total sentences in the business category: {sum([len(tokenizer.sentencize(d)) for d in corpus_split[0]])}')
+print(f'Total sentences in the entertainment category: {sum([len(tokenizer.sentencize(d)) for d in corpus_split[1]])}')
+print(f'Total sentences in the politics category: {sum([len(tokenizer.sentencize(d)) for d in corpus_split[2]])}')
+print(f'Total sentences in the sport category: {sum([len(tokenizer.sentencize(d)) for d in corpus_split[3]])}')
+print(f'Total sentences in the tech category: {sum([len(tokenizer.sentencize(d)) for d in corpus_split[4]])}')
+print(f'Total sentences in the entire corpus: {sum([len(tokenizer.sentencize(d)) for d in corpus])}\n')
 
-def tokens_count(corpus):
-    doc = nlp(corpus)
+# Tokens
+print(f'Total tokens in the business category: {sum([len(tokenizer.tokenize(d)) for d in corpus_split[0]])}')
+print(f'Total tokens in the entertainment category: {sum([len(tokenizer.tokenize(d)) for d in corpus_split[1]])}')
+print(f'Total tokens in the politics category: {sum([len(tokenizer.tokenize(d)) for d in corpus_split[2]])}')
+print(f'Total tokens in the sport category: {sum([len(tokenizer.tokenize(d)) for d in corpus_split[3]])}')
+print(f'Total tokens in the tech category: {sum([len(tokenizer.tokenize(d)) for d in corpus_split[4]])}')
+print(f'Total tokens in the entire corpus: {sum([len(tokenizer.tokenize(d)) for d in corpus])}\n')
 
-    return len(list(doc))
-
-
-print(f'Total tokens in the business category: {tokens_count(corpus_split[0])}')
-print(f'Total tokens in the entertainment category: {tokens_count(corpus_split[1])}')
-print(f'Total tokens in the politics category: {tokens_count(corpus_split[2])}')
-print(f'Total tokens in the sport category: {tokens_count(corpus_split[3])}')
-print(f'Total tokens in the tech category: {tokens_count(corpus_split[4])}')
-print(f'Total tokens in the entire corpus: {tokens_count(full_corpus)}\n')
-
-
-def vocabulary_count(corpus):
-    doc = nlp(corpus)
-
-    return len(set([token.lemma_ for token in doc]))
-
-
-print(f'Vocabulary for the business category: {vocabulary_count(corpus_split[0])}')
-print(f'Vocabulary for the entertainment category: {vocabulary_count(corpus_split[1])}')
-print(f'Vocabulary for the politics category: {vocabulary_count(corpus_split[2])}')
-print(f'Vocabulary for the sport category: {vocabulary_count(corpus_split[3])}')
-print(f'Vocabulary for the tech category: {vocabulary_count(corpus_split[4])}')
-print(f'Vocabulary for the entire corpus: {vocabulary_count(full_corpus)}')
+# Vocabulary
+print(f'Vocabulary for the business category: {sum([len(set(tokenizer.tokenize(d.lower()))) for d in corpus_split[0]])}')
+print(f'Vocabulary for the entertainment category: {sum([len(set(tokenizer.tokenize(d.lower()))) for d in corpus_split[1]])}')
+print(f'Vocabulary for the politics category: {sum([len(set(tokenizer.tokenize(d.lower()))) for d in corpus_split[2]])}')
+print(f'Vocabulary for the sport category: {sum([len(set(tokenizer.tokenize(d.lower()))) for d in corpus_split[3]])}')
+print(f'Vocabulary for the tech category: {sum([len(set(tokenizer.tokenize(d.lower()))) for d in corpus_split[4]])}')
+print(f'Vocabulary for the entire corpus: {sum([len(set(tokenizer.tokenize(d.lower()))) for d in corpus])}')
